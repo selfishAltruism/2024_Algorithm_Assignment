9d4c65bc37d05d04fcd7243606783cc9ce33ae4e
selfishAltruism <officialkyus@gmail.com>
2024-05-14 20:46:19 +0900

    feat: fix read line error

--- assignment3/lcs.py
+++ assignment3/lcs.py
@@ -1,8 +1,8 @@
 import sys
 
-def lcs(str1, str2):
-    m = len(str1)
-    n = len(str2)
+def lcs(dna1, dna2):
+    m = len(dna1)
+    n = len(dna2)
 
     #table initialization
     dp = [[0] * (n + 1) for _ in range(m + 1)]
@@ -10,7 +10,7 @@ def lcs(str1, str2):
     #lcs calculation
     for i in range(1, m + 1):
         for j in range(1, n + 1):
-            if str1[i - 1] == str2[j - 1]:
+            if dna1[i - 1] == dna2[j - 1]:
                 dp[i][j] = dp[i - 1][j - 1] + 1
             else:
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
@@ -23,8 +23,8 @@ def lcs(str1, str2):
     i = m
     j = n
     while i > 0 and j > 0:
-        if str1[i - 1] == str2[j - 1]:
-            lcs[lcs_length - 1] = str1[i - 1]
+        if dna1[i - 1] == dna2[j - 1]:
+            lcs[lcs_length - 1] = dna1[i - 1]
             i -= 1
             j -= 1
             lcs_length -= 1
@@ -43,7 +43,8 @@ output = open(sys.argv[2],"w")
 length = int(input.readline())
 dna_list = []
 while True:
-    line = input.readline()
+    #remove \n
+    line = input.readline().strip()
     if not line: 
         break
     dna_list.append(list(line))
@@ -63,8 +64,8 @@ for i in range(0, len(dna_list)):
                 most_len = len(result)
                 most_result = result
     
-    if(i == len(dna_list)): output.write(str(most_index + 1) + '\t' + str(most_len) + '\t' + most_result)
-    else: output.write(str(most_index + 1) + '\t' + str(most_len - 1) + '\t' + most_result)
+    output.write(str(most_index + 1) + '\t' + str(most_len) + '\t' + most_result + '\n')
+
 
 #close file
 input.close()
