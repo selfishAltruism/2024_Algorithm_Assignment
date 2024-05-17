55344b58320053d2b66a01564b7a067bb875b8ec
selfishAltruism <officialkyus@gmail.com>
2024-05-17 14:31:10 +0900

    feat: add annotation, update variable identifier

--- assignment3/lcs.py
+++ assignment3/lcs.py
@@ -1,27 +1,33 @@
 import sys
 
 def lcs(dna1, dna2):
-    m = len(dna1)
-    n = len(dna2)
+    dna1_len = len(dna1)
+    dna2_len = len(dna2)
 
-    #table initialization
-    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    #lcs dp table initialization
+    dp = []
+    for i in range(dna1_len + 1):
+        row = [0] * (dna2_len + 1)
+        dp.append(row)
 
-    #lcs calculation
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
+    #lcs dp table population
+    for i in range(1, dna1_len + 1):
+        for j in range(1, dna2_len + 1):
+            #if dna1 element and dna2 element are the same, +1
             if dna1[i - 1] == dna2[j - 1]:
                 dp[i][j] = dp[i - 1][j - 1] + 1
+            #if dna1 element and dna2 element are not the same, select max
             else:
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
-    #lcs backtracking
-    lcs_length = dp[m][n]
+    #lcs backtrack initialization
+    lcs_length = dp[dna1_len][dna2_len]
     lcs = [""] * (lcs_length + 1)
     lcs[lcs_length] = ""
 
-    i = m
-    j = n
+    #lcs backtracking
+    i = dna1_len
+    j = dna2_len
     while i > 0 and j > 0:
         if dna1[i - 1] == dna2[j - 1]:
             lcs[lcs_length - 1] = dna1[i - 1]
@@ -42,12 +48,14 @@ output = open(sys.argv[2],"w")
 
 length = int(input.readline())
 dna_list = []
+
 while True:
     #remove \n
     line = input.readline().strip()
     if not line: 
         break
     dna_list.append(list(line))
+
 
 #run lcs
 for i in range(0, len(dna_list)):
