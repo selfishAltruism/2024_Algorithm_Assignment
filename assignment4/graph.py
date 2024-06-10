322b2da2f85985f6301ef629f1f3981860ff6d03
selfishAltruism <officialkyus@gmail.com>
2024-06-10 17:44:17 +0900

    feat: add vertex & edge setting system

--- assignment4/graph.py
+++ assignment4/graph.py
@@ -1,6 +1,19 @@
 import sys
 from collections import defaultdict
 
+BIG_NUM = 10**100
+NULL = -1
+
+class Vertex:
+    def __init__(self, vertex_id):
+        self.id = vertex_id
+        self.key = BIG_NUM
+        self.before = NULL
+
+    def __repr__(self):
+        return f"Vertex(id={self.id}, key={self.key}, before={self.before})"
+    
+
 #open file
 input_sp = open(sys.argv[1],"r")
 input_mst = open(sys.argv[2],"r")
@@ -10,7 +23,8 @@ output_mst = open(sys.argv[4],"w")
 #mst
 num_vertices, num_edges, start_vertex_id = map(int, input_mst.readline().split('\t'))
 
-graph = defaultdict(list)
+graph_mst = defaultdict(list)
+vertices_mst = {}
 
 while True:
     #remove \n
@@ -20,8 +34,16 @@ while True:
     u, v, weight = map(int, line.split('\t'))
 
     #because of undirected graph
-    graph[u].append((v, weight))
-    graph[v].append((u, weight))
+    graph_mst[u].append((v, weight))
+    graph_mst[v].append((u, weight))
+
+    #add vertex
+    if u not in vertices_mst:
+        vertices_mst[u] = Vertex(u)
+    if v not in vertices_mst:
+        vertices_mst[v] = Vertex(v)
+
+
 
 
 #close file
